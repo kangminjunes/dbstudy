e6ea65a072368ec8164f0b8bcc122c36771db1f9
kangminjunes <1999koco@naver.com>
2023-07-07 17:42:13 +0900

    20230707

---
+++ DAY_07_01_서브쿼리.sql
@@ -0,0 +1,130 @@
+/*
+    서브쿼리(sub query)
+    1. 메인 쿼리에 포함되는 하위쿼리를 서브쿼리라고 한다.
+    2. 서브쿼리를 먼저 실행해서 그 결과를 메인쿼리에 전달한다.
+    3. 종류
+        1) SELECT 절 : 스칼라 서브쿼리
+        2)   FROM 절 : 인라이 뷰 (INLINE VIEW)
+        3)  WHERE 절 : 단일 행 서브쿼리 (결과가 1개)   
+                       다중 행 서브쿼리 (결과가 N개)
+*/
+
+/*
+    단일 행 서브쿼리(single row sub query)
+    1. 결과가 1행이다. (1개이다.)
+    2. 단일 행 서브쿼리인 경우
+        1) WHERE 절에서 사용한 칼럼이 PK 또는 UNIQUE 칼럼인 경우
+        2) 통계 함수를 사용한 경우 
+    3. 단일 행 서브쿼리 연산자
+        =, !=, >, >=, <, <=
+  
+    다중 행 서브쿼리 (multiple row sub query)
+    1. 결과가 N행이다.
+    2. 다중 행 서브쿼리 연산자
+        IN, ANY, ALL 등
+*/
+
+/* WHERE 절의 서브쿼리 */
+-- 1. 사원번호가 101인 사원의 직업과 동일한 직업을 가진 사원을 조회하기
+-- SELECT *
+--   FROM EMPLOYEES
+--  WHERE JOB_ID = (사원번호가 101인 사원의 직업);
+
+SELECT *
+  FROM EMPLOYEES
+ WHERE JOB_ID = (SELECT JOB_ID
+                  FROM EMPLOYEES 
+                 WHERE EMPLOYEE_ID =101);
+
+-- 2. 부서명이 'IT'인 부서에 근무하는 사원 조회하기
+
+-- SELECT *
+--   FROM EMPLOYEES
+--  WHERE DEPARTMENT_ID = (부서명이 'IT'인 부서의 부서번호)
+
+SELECT *
+  FROM EMPLOYEES
+ WHERE DEPARTMENT_ID IN (SELECT DEPARTMENT_ID
+                          FROM DEPARTMENTS
+                         WHERE DEPARTMENT_NAME = 'IT'); --서브쿼리의 DEPARTMENT_NAME 칼럼은 중복이 있을 수 있으므로  다중행 서브쿼리로 처리한다.
+                         
+-- 3. 'Seattle'에서 근무하는 사원 조회하기
+
+SELECT *
+  FROM EMPLOYEES
+ WHERE DEPARTMENT_ID IN (SELECT DEPARTMENT_ID
+                          FROM DEPARTMENTS
+                         WHERE LOCATION_ID  IN (SELECT LOCATION_ID
+                                                 FROM LOCATIONS
+                                                WHERE CITY = 'Seattle'));
+
+-- 4. 연봉이 가장 높은 사원 조회하기
+SELECT *
+  FROM EMPLOYEES
+ WHERE SALARY = (SELECT MAX (SALARY)
+                   FROM EMPLOYEES);
+                   
+-- 5. 가장 먼저 입사한 사원 조회하기
+SELECT *
+  FROM EMPLOYEES
+ WHERE HIRE_DATE = (SELECT MIN(HIRE_DATE) 
+                      FROM EMPLOYEES);
+ 
+
+-- 6. 평균 연봉 이상을 받는 사원 조회하기
+SELECT *
+  FROM EMPLOYEES
+ WHERE SALARY  >=  (SELECT AVG(SALARY)
+                     FROM EMPLOYEES);
+
+
+/* FROM 문의 서브쿼리 */
+-- 사용 주된 이유 : 실행순서 떄문이다.
+-- FROM절이 실행순서가 1번 가장 먼저 실행하기 위해서 사용한다.
+
+-- 1. 연봉이 3번째로 높은 사원 조회하기
+--      1) 높은 연봉 순으로 정렬한다.
+--      2) 정렬 결과에 행 번호를 붙인다.
+--      3) 형 번호 3을 가져온다.
+
+
+ 
+-- 2. 연봉 순위 11~ 20번쨰 사원 조회하기
+
+SELECT RN, EMPLOYEE_ID
+  FROM (SELECT ROW_NUMBER() OVER(ORDER BY SALARY DESC) AS RN, EMPLOYEE_ID
+          FROM EMPLOYEES)
+ WHERE RN BETWEEN 11 AND 20;
+ 
+-- 3. 21~30번쨰로 입사한 사원 조회하기 HIRE_DATE
+SELECT HD, EMPLOYEE_ID
+  FROM (SELECT ROW_NUMBER() OVER(ORDER BY HIRE_DATE DESC) AS HD, EMPLOYEE_ID
+          FROM EMPLOYEES)
+ WHERE HD BETWEEN 21 AND 30;
+ 
+ /* SELECT 절의 서브쿼리 */
+ 
+ -- 부서번호가 50인 부서에 근무하는 사원번호, 사원명, 부서명 조회하기(비상관)
+ 
+SELECT EMPLOYEE_ID ,
+       FIRST_NAME ,
+       LAST_NAME ,
+        (SELECT DEPARTMENT_NAME
+           FROM DEPARTMENTS
+          WHERE DEPARTMENT_ID = 50) AS DEPT_NAME
+  FROM EMPLOYEES
+ WHERE DEPARTMENT_ID = 50;
+
+-- 서브쿼리와 메인쿼리가 상관없는것이 비상관커리 이다.
+-- 메인쿼리 실행시 서브 쿼리 영향 없고 반대로 실행하여도 없기 떄문이다.
+
+-- 부서번호가 50인 부서에 근무하는 사원번호, 사원명, 부서명 조회하기(상관)
+SELECT E.EMPLOYEE_ID ,
+       E.FIRST_NAME,
+       E.LAST_NAME,
+        (SELECT D.DEPARTMENT_NAME
+           FROM DEPARTMENTS D
+          WHERE D.DEPARTMENT_ID = E.DEPARTMENT_ID
+            AND D.DEPARTMENT_ID = 50) AS DEPT_NAME
+  FROM EMPLOYEES E;
+-- 상관 커리
---
+++ 연습문제_서브쿼리.sql
@@ -0,0 +1,69 @@
+--DEPARTMENT_T ,EMPLOYEE_T
+
+-- 1. 사원번호가 1001인 사원과 동일한 직급(POSITION)을 가진 사원을 조회하시오.
+SELECT *
+  FROM EMPLOYEE_T
+ WHERE POSITION = (SELECT POSITION
+                     FROM EMPLOYEE_T
+                    WHERE EMP_NO= 1001);
+
+-- 2. 부서번호가 2인 부서와 동일한 지역에 있는 부서를 조회하시오.
+SELECT *
+  FROM EMPLOYEE_T
+ WHERE DEPART IN (SELECT DEPART
+                   FROM EMPLOYEE_T
+                  WHERE DEPARTMENT_T  IN (SELECT LOCATION
+                                     FROM  DEPARTMENT_T
+                                    WHERE  DEPT_NO = '2'));
+ 
+-- 3. 가장 높은 급여를 받는 사원을 조회하시오.
+SELECT *
+  FROM EMPLOYEE_T
+ WHERE SALARY = (SELECT MAX (SALARY)
+                   FROM EMPLOYEE_T);
+-- 4. 평균 급여 이하를 받는 사원을 조회하시오.
+SELECT *
+  FROM EMPLOYEE_T
+ WHERE SALARY  <= (SELECT AVG(SALARY)
+                     FROM EMPLOYEE_T);
+
+-- 5. 평균 근속 개월 수 이상을 근무한 사원을 조회하시오.
+SELECT *
+ FROM EMPLOYEE_T
+WHERE HIRE_DATE >= (SELECT AVG(HIRE_DATE)
+                      FROM EMPLOYEE_T);
+
+-- 6. 부서번호가 2인 부서에 근무하는 사원들의 직급과 일치하는 사원을 조회하시오.
+SELECT *
+  FROM EMPLOYEE_T
+ WHERE DEPART IN (SELECT DEPART
+                    FROM DEPARTMENT_T
+                    WHERE DEPT_NO = '2');
+ 
+-- 7. 부서명이 '영업부'인 부서에 근무하는 사원을 조회하시오.
+SELECT *
+  FROM EMPLOYEE_T
+ WHERE  DEPART IN (SELECT DEPART
+                    FROM DEPARTMENT_T
+                   WHERE DEPT_NAME = '영업부');
+
+-- 8. 직급이 '과장'인 사원들이 근무하는 부서 정보를 조회하시오.
+SELECT  DEPT_NO,
+        DEPT_NAME,
+        (SELECT DEPART
+          FROM EMPLOYEE_T
+        WHERE DEPART = '과장')  AS DEPT_NAME
+  FROM EMPLOYEE_T
+ WHERE DEPART= '과장';
+
+-- 9. '영업부'에서 가장 높은 급여를 받는 사람보다 더 높은 급여를 받는 사원을 조회하시오.
+SELECT *
+  FROM EMPLOYEE_T
+ WHERE SALARY = (SELECT MAX (SALARY)
+                   FROM EMPLOYEE_T);
+
+-- 10. 3 ~ 4번째로 입사한 사원을 조회하시오.
+SELECT HD, EMP_NO
+ FROM (SELECT ROW_NUMBER() OVER(ORDER BY HIRE_DATE DESC) HD, EMP_NO
+         FROM EMPLOYEE_T)
+ WHERE HD BETWEEN 3 AND 4;
